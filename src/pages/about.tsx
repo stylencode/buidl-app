07a4dd2f320f384918fd1e8c11fb5eab15ad7499
Fuad Talybov <fuad.talybov@gmail.com>
2024-02-25 22:00:15 -0700

    about updated

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,20 +1,10 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+//import Image from "next/image";
+//import { Inter } from "next/font/google";
 
 export default function About() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-         <p> hello world </p>
-       
-      </div>
+    
+    <p> hello world </p>
 
-     
-
-    </main>
   );
 }
